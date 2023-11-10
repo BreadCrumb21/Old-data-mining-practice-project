0bfc105d25e1f82a0f67410b7d3db1a68c658121
Kaden R <kaden2ramirez3@gmail.com>
2023-11-10 15:27:30 -0600

    Finished Hot Encoder and data
    Finished the hot encoder. Then I encoded the training data with the hot encoder. It is in sparse format instead of traditional hot encoded format as the data was too big to transform into the traditional 1 and 0s with each column.

--- hotEncoder.py
+++ hotEncoder.py
@@ -1,15 +1,18 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
+from scipy.sparse import csr_matrix
+import dask.dataframe as dd
+
 from numpy import savetxt
 
 # Import the data required from a CSV file named 'adultDataCorrected.csv' into a Pandas DataFrame
-data = pd.read_csv('train.csv')
+data = pd.read_csv('train.csv/train.csv')
 
 # Convert categorical columns to category data type and create new numerical columns
 # These new numerical columns will contain the category codes of the original categorical data
 data['Dates'] = data['Dates'].astype('category')
-data['Catagory'] = data['Catagory'].astype('category')
+data['Category'] = data['Category'].astype('category')
 data['Descript'] = data['Descript'].astype('category')
 data['DayOfWeek'] = data['DayOfWeek'].astype('category')
 data['PdDistrict'] = data['PdDistrict'].astype('category')
@@ -18,42 +21,31 @@ data['Address'] = data['Address'].astype('category')
 data['X'] = data['X'].astype('category')
 data['Y'] = data['Y'].astype('category')
 
-# data['Dates_new'] = data['Dates'].cat.codes
-# data['Catagory_new'] = data['Catagory'].cat.codes
-# data['Descript_new'] = data['Descript'].cat.codes
-# data['DayOfWeek_new'] = data['DayOfWeek'].cat.codes
-# data['PdDistrict_new'] = data['PdDistrict'].cat.codes
-# data['Resolution_new'] = data['Resolution'].cat.codes
-# data['Address_new'] = data['Address'].cat.codes
-# data['X_new'] = data['X'].cat.codes
-# data['Y_new'] = data['Y'].cat.codes
+data['Dates_new'] = data['Dates'].cat.codes
+data['Category_new'] = data['Category'].cat.codes
+data['Descript_new'] = data['Descript'].cat.codes
+data['DayOfWeek_new'] = data['DayOfWeek'].cat.codes
+data['PdDistrict_new'] = data['PdDistrict'].cat.codes
+data['Resolution_new'] = data['Resolution'].cat.codes
+data['Address_new'] = data['Address'].cat.codes
+data['X_new'] = data['X'].cat.codes
+data['Y_new'] = data['Y'].cat.codes
 
 # Create an instance of the OneHotEncoder
 encoder = OneHotEncoder()
 
 # Perform one-hot encoding on selected columns and convert the result to a DataFrame
-enc_data = pd.DataFrame(encoder.fit_transform(data[['Dates_new', 'Catagory_new',
-                                                    'Descript_new', 'DayOfWeek_new',
-                                                    'PdDistrict_new', 'Resolution_new', 'Address_new',
-                                                    'X_new', 'Y_new']]).toarray())
+enc_data_sparse = csr_matrix(encoder.fit_transform(data[['Dates_new', 'Category_new',
+                                                         'Descript_new', 'DayOfWeek_new',
+                                                         'PdDistrict_new', 'Resolution_new', 'Address_new',
+                                                         'X_new', 'Y_new']]))
 
-# Join the original data DataFrame with the encoded DataFrame to create a new DataFrame
-New_df = data.join(enc_data)
+# Convert Pandas DataFrame to Dask DataFrame
+dask_data = dd.from_pandas(data, npartitions=10)
 
-# # Print the new DataFrame
-print(New_df)
+# Apply one-hot encoding using Dask
+dask_encoded = dask_data[['Dates_new', 'Category_new', 'Descript_new', 'DayOfWeek_new',
+                          'PdDistrict_new', 'Resolution_new', 'Address_new', 'X_new', 'Y_new']].categorize().compute()
 
-# # Extract only the numerical part of New_df to a CSV file
-# numerical_data = New_df.select_dtypes(include=[np.number])
-
-# # Get the column labels (column names) of the numerical_data DataFrame
-# column_labels = numerical_data.columns.tolist()
-
-# # Convert New_df to a NumPy array
-# compatibleArr = numerical_data.values
-
-# # Save the NumPy array to a CSV file named 'hotData.csv'
-# savetxt('hotData.csv', compatibleArr, delimiter=',')
-
-# # Print the column labels
-# print("Column Labels:", column_labels)
+# Save the result to a CSV file
+dask_encoded.to_csv('hotData.csv', index=False)
