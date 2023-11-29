b7063c3861c1e7d1f193a79bc58efb79e827db37
Jturner-code <54911366+Jturner-code@users.noreply.github.com>
2023-11-29 10:21:40 -0600

    Update NBC.py
    99% accuracy

--- NBC.py
+++ NBC.py
@@ -32,29 +32,41 @@ enc.fit(test_data[["Dates","Category","Descript","DayOfWeek","PdDistrict","Resol
 test_data[["Dates","Category","Descript","DayOfWeek","PdDistrict","Resolution","Address","X","Y"]] = enc.fit_transform(
     test_data[["Dates","Category","Descript","DayOfWeek","PdDistrict","Resolution","Address","X","Y"]])
 
+col_names = ['Dates','Category','Descript','DayOfWeek','PdDistrict','Resolution','Address','X','Y']
+# load dataset
+submit_data = pd.read_csv(r"C:\Users\Jaden\OneDrive\Desktop\CS\DM\Data mining project\test.csv", header=None, names=col_names, dtype=str)
+# print(test_data.head())
+# Identify columns with mixed data types
+mixed_type_columns = [col for col in test_data.columns if test_data[col].apply(lambda x: type(x) == str).any()]
 
-x = test_data.drop(columns=['Dates','Descript','DayOfWeek','PdDistrict','Resolution','Address','X','Y'])
-y = test_data['Category']
+# Convert mixed data type columns to a consistent data type (e.g., str)
+for col in mixed_type_columns:
+    submit_data[col] = test_data[col].astype(str)
 
-x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.33, random_state=125
-)
+enc = OrdinalEncoder()
+#encoded_data = pd.DataFrame(enc.fit_transform(test_data))
+#print(encoded_data.head())
+enc.fit(test_data[["Dates","Category","Descript","DayOfWeek","PdDistrict","Resolution","Address","X","Y"]].astype(str))
+submit_data[["Dates","Category","Descript","DayOfWeek","PdDistrict","Resolution","Address","X","Y"]] = enc.fit_transform(
+    submit_data[["Dates","Category","Descript","DayOfWeek","PdDistrict","Resolution","Address","X","Y"]])
 
+# Split the data into features (x) and target variable (y)
+x_train = test_data.drop(columns=['Dates', 'Descript', 'DayOfWeek', 'PdDistrict', 'Resolution', 'Address', 'X', 'Y'])
+y_train = test_data['Category']
+
+# Split the data into training and testing sets
+x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
+
+# Create and train the model
 model = GaussianNB()
 model.fit(x_train, y_train)
 
-from sklearn.metrics import (
-    accuracy_score,
-    confusion_matrix,
-    ConfusionMatrixDisplay,
-    f1_score,
-    classification_report,
-)
-
+# Predict on the test set
 y_pred = model.predict(x_test)
 
-accuray = accuracy_score(y_pred, y_test)
-f1 = f1_score(y_pred, y_test, average="weighted")
+# Calculate accuracy and F1 score
+accuracy = accuracy_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred, average="weighted")
 
-print("Accuracy:", accuray)
+print("Accuracy:", accuracy)
 print("F1 Score:", f1)
