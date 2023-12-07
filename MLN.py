182c005aed7e43e16ce06fb5ac06854688ca44f6
CJBrown314 <CJamesBrown314@gmail.com>
2023-12-07 11:08:05 -0600

    Updating MLN algorithm to make predictions on actual data.

--- MLN.py
+++ MLN.py
@@ -1,8 +1,11 @@
 import math
+from random import randint
 
 import numpy as np
 import pandas
+import pandas as pd
 from scipy import stats
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import log_loss
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
@@ -10,7 +13,7 @@ from sklearn.preprocessing import StandardScaler
 
 from DataConstruction import construct_cat_value
 
-train_data = construct_cat_value('./train.csv/train.csv', ['Category', 'Descript', 'DayOfWeek', 'PdDistrict',
+train_data = construct_cat_value('./train.csv/train.csv', ['Descript', 'DayOfWeek', 'PdDistrict',
                                                            'Resolution'])
 
 # Removing "### Block of" from addresses
@@ -51,10 +54,40 @@ x_test['R'] = std.transform(x_test[['R']])
 x_train['theta'] = std.fit_transform(x_train[['theta']])
 x_test['theta'] = std.transform(x_test[['theta']])
 
+test_data = construct_cat_value('./test.csv/test.csv', ['DayOfWeek', 'PdDistrict'])
+
+# Removing "### Block of" from addresses
+test_data['Address'] = test_data['Address'].str.replace(r'[0-9]+ Block of ', '', regex=True)
+test_data['Address'] = test_data['Address'].astype('category')
+test_data['Address'] = test_data['Address'].cat.codes
+
+# Split date into separate hours, minutes, etc.
+test_data['Dates'] = pandas.to_datetime(test_data['Dates'])
+test_data['Hours'] = test_data['Dates'].dt.hour
+test_data['Minutes'] = test_data['Dates'].dt.minute
+test_data['Month'] = test_data['Dates'].dt.month
+test_data['Year'] = test_data['Dates'].dt.year
+
+# Convert X & Y into polar coordinates, just to have another variable to play with
+test_data['R'] = np.sqrt(np.power(test_data['X'], 2), np.power(test_data['Y'], 2))
+test_data['theta'] = np.arctan(np.divide(test_data['Y'], test_data['X']))
+
+# Split data
+# Standardize the coordinate variables
+std = StandardScaler()
+test_data['X'] = std.fit_transform(test_data[['X']])
+test_data['Y'] = std.fit_transform(test_data[['Y']])
+test_data['R'] = std.fit_transform(test_data[['R']])
+test_data['theta'] = std.fit_transform(test_data[['theta']])
+
 # Set up and train the MLP classifier
-mln = MLPClassifier(max_iter=1000, verbose=True)
+mln = RandomForestClassifier(random_state=42, n_estimators=100, verbose=2, min_samples_split=randint(2, 20), min_samples_leaf=randint(1, 20),
+                             max_depth=5)
 mln.fit(x_train, y_train)
 
+test_data = test_data.drop(['Id', 'Dates', 'Address'], axis=1)
+
 # Predict results and calculate the Log Loss
-y_pred = mln.predict_proba(x_test)
-print(log_loss(y_test, y_pred))
+y_pred = mln.predict_proba(test_data)
+prediction = pd.DataFrame(y_pred, columns=mln.classes_)
+prediction.to_csv('results.csv')
